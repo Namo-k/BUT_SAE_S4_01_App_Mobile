8773611bb6a705234ce26d85575436ec0d908e17
Namo-k <121117705+Namo-k@users.noreply.github.com>
2024-02-19 13:58:00 +0100

    Squelette projet

---
+++ app/src/main/java/fr/iut/sae_s4_01_app_mobile/MainActivity.java
@@ -0,0 +1,14 @@
+package fr.iut.sae_s4_01_app_mobile;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "SAE_S4_01_App_Mobile"
+include(":app")
+ 
