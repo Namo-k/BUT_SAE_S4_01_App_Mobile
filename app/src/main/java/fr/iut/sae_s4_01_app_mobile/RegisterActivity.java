13b0ac520cc8fb49c74347ec38b4af0bc480e631
isradh <128903699+isradh@users.noreply.github.com>
2024-02-22 23:43:49 +0100

    design inscrption, gestion erreur, contrainte de saisie

--- app/src/main/java/fr/iut/sae_s4_01_app_mobile/RegisterActivity.java
+++ app/src/main/java/fr/iut/sae_s4_01_app_mobile/RegisterActivity.java
@@ -1,6 +1,12 @@
 package fr.iut.sae_s4_01_app_mobile;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -9,5 +15,71 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+
+        Button passersuite = findViewById(R.id.loginBtn);
+        passersuite.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText nom = findViewById(R.id.prenom);
+                EditText prenom = findViewById(R.id.prenom1);
+                EditText jour = findViewById(R.id.Djour);
+                EditText mois = findViewById(R.id.Dmois);
+                EditText annee = findViewById(R.id.annee);
+                String nom_ = nom.getText().toString();
+                String prenom_ = prenom.getText().toString();
+                String jour_ = jour.getText().toString();
+                String mois_ = mois.getText().toString();
+                String annee_ = annee.getText().toString();
+                if (nom_.isEmpty() || prenom_.isEmpty() || jour_.isEmpty() || mois_.isEmpty() || annee_.isEmpty()) {
+                    Toast.makeText(RegisterActivity.this, "Veuillez remplir tous le champs", Toast.LENGTH_SHORT).show();
+                } else {
+                    if (!verifjour(jour_)) {
+                        Toast.makeText(RegisterActivity.this, "Le jour saisie n'est pas conforme", Toast.LENGTH_SHORT).show();
+                    }else if(!verifmois(mois_)){
+                        Toast.makeText(RegisterActivity.this, "Le mois saisie n'est pas conforme", Toast.LENGTH_SHORT).show();
+                    }else if(!verifanne(annee_)){
+                        Toast.makeText(RegisterActivity.this, "L'année saisie n'est pas conforme", Toast.LENGTH_SHORT).show();
+                    }else {
+                        Intent intent = new Intent(RegisterActivity.this, RegisterActivity2.class);
+                        startActivity(intent);
+                    }
+                }
+            }
+        });
     }
+
+    public boolean verifanne(String annee){
+        int year;
+        if (annee.length() == 4) {
+            year = Integer.parseInt(annee);
+            if (year >= 1900 && year <= 2024) {
+                return true;
+            }
+        }
+        return false;
+    }
+    public boolean verifmois(String mois){
+        int month;
+        if (mois.length() == 2) {
+            month = Integer.parseInt(mois);
+            if (month >= 01 && month <= 12) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean verifjour(String jour){
+        int day;
+        if (jour.length() == 2) {
+            day = Integer.parseInt(jour);
+            if (day >= 1 && day <= 31) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+
 }
--- app/src/main/java/fr/iut/sae_s4_01_app_mobile/RegisterActivity2.java
+++ app/src/main/java/fr/iut/sae_s4_01_app_mobile/RegisterActivity2.java
@@ -1,15 +1,48 @@
 package fr.iut.sae_s4_01_app_mobile;
 
-import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class RegisterActivity2 extends AppCompatActivity {
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register2);
+
+
+        Button validateButton = findViewById(R.id.loginBtn);
+        validateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText emailEditText = findViewById(R.id.email);
+                EditText passwordEditText = findViewById(R.id.mdp);
+                String password = passwordEditText.getText().toString().trim();
+                String email = emailEditText.getText().toString().trim();
+                if (!isValidEmail(email)){
+                    Toast.makeText(RegisterActivity2.this, "Adresse email non valide", Toast.LENGTH_SHORT).show();
+                }else if (!isPasswordValid(password)) {
+                    Toast.makeText(RegisterActivity2.this, "Le mot de passe doit avoir au moins 8 caractères, une majuscule et un chiffre.", Toast.LENGTH_SHORT).show();
+                }
+
+
+    } });
+
+    }
+
+    private boolean isValidEmail(String email) {
+        return !TextUtils.isEmpty(email) && Patterns.EMAIL_ADDRESS.matcher(email).matches();
+    }
+
+    private boolean isPasswordValid(String password) {
+        return password.length() >= 8 && password.matches(".*[A-Z].*") && password.matches(".*\\d.*");
     }
 }
