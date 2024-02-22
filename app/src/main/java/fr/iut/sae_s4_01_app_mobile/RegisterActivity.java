8c1d2af4fd7f3d82129a5dd971b7916f9bc29d49
oum <olamrabet12@gmail.com>
2024-02-22 18:50:56 +0100

    PAGE CONNEXION , BACK + FRONT  , page inscription front

--- app/src/main/java/fr/iut/sae_s4_01_app_mobile/RegisterActivity.java
+++ app/src/main/java/fr/iut/sae_s4_01_app_mobile/RegisterActivity.java
@@ -1,6 +1,9 @@
 package fr.iut.sae_s4_01_app_mobile;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -11,5 +14,14 @@ public class RegisterActivity extends AppCompatActivity {
         setContentView(R.layout.activity_register);
 
 
+        Button loginBtn = findViewById(R.id.loginBtn);
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(RegisterActivity.this, RegisterActivity2.class);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/fr/iut/sae_s4_01_app_mobile/RegisterActivity2.java
@@ -0,0 +1,17 @@
+package fr.iut.sae_s4_01_app_mobile;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class RegisterActivity2 extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register2);
+
+
+    }
+}
